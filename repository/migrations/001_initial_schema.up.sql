6864634b8757fb9e74619a1257e1e17eb6626b1c
JonasHiltl <jonashiltl2003@gmail.com>
2022-06-07 20:22:56 +0200

    fix schema

--- repository/migrations/001_initial_schema.up.sql
+++ repository/migrations/001_initial_schema.up.sql
@@ -4,11 +4,11 @@ CREATE TYPE type AS ENUM ('USER', 'ADMIN', 'DEV', 'COMPANY');
 CREATE TABLE accounts (
     id char(27) PRIMARY KEY,
     email TEXT NOT NULL,
-    email_verified BOOLEAN DEFAULT false NOT NULL,
+    email_verified BOOLEAN DEFAULT false,
     email_code TEXT,
     password_hash TEXT NOT NULL,
     provider provider DEFAULT NULL,
-    type type NULL DEFAULT "USER"
+    type type NULL DEFAULT 'USER'
 );
 
 CREATE UNIQUE INDEX email_idx ON accounts (email);
